148b786e65319caaf09f6bbe5f5ecf577f59e1c9
Vishal Kumar <vishal.kumar@xcitium.com>
2026-03-13 07:39:22 +0530

    user/me api call fixed

--- src/common/config/jwt.config.ts
+++ src/common/config/jwt.config.ts
@@ -3,6 +3,6 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('jwt', () => {
   return {
     secret: process.env.JWT_SECRET,
-    accessTokenTtl: process.env.JWT_ACCESS_TOKEN_TTL,
+    accessTokenTtl: parseInt(process.env.JWT_ACCESS_TOKEN_TTL, 10),
   };
 });
